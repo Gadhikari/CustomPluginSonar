5ff3bbe51194d43040403b94d1cbb6730841b4bc
gaurav adhikari <gaurav26adhikari@gmail.com>
2016-03-03 16:20:10 +0530

    Customplugin

---
+++ javascript-custom-rules/src/test/java/org/sonar/samples/javascript/ForbiddenFunctionUseCheckTest.java
@@ -0,0 +1,20 @@
+package org.sonar.samples.javascript;
+
+
+/**
+ * Test class to test the check implementation.
+ */
+public class ForbiddenFunctionUseCheckTest {
+
+/*  @Test
+  public void test() throws Exception {
+    SourceFile file = scanFile("src/test/resources/checks/forbiddenFunctionUseCheck.js", new ForbiddenFunctionUseCheck());
+
+    CheckMessagesVerifier.verify(file.getCheckMessages())
+      .next().atLine(1).withMessage("Remove the usage of this forbidden function.")
+      .next().atLine(2)
+      .next().atLine(4)
+      .next().atLine(5);
+  }*/
+
+}
